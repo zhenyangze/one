3766dcdc689a7fc9b74ece7c0db00962b57c8dfd
tanszhe <1018595261@qq.com>
2018-05-31 11:36:18 +0800

    日志调试

--- One/Exceptions/HttpException.php
+++ One/Exceptions/HttpException.php
@@ -10,6 +10,6 @@ class HttpException extends \Exception
     public function __construct(string $message = "", int $code = 0,  $previous = null)
     {
         parent::__construct($message,$code);
-        Log::error($message,'http',1);
+        Log::error($message,'http',3);
     }
 }
