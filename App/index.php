19c2b1db5fb7c0a5b35f3df9fe8c81146f472a68
DlenoDing <dleno@126.com>
2018-10-10 16:03:23 +0800

    http int

--- App/index.php
+++ App/index.php
@@ -1,24 +1,30 @@
 <?php
 
-define('_APP_PATH_',__DIR__);
+define('_APP_PATH_', __DIR__);
 
-define('_APP_PATH_VIEW_',__DIR__.'/View');
+define('_APP_PATH_VIEW_', __DIR__ . '/View');
 
-require __DIR__.'/../One/run.php';
+require __DIR__ . '/../One/run.php';
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-require _APP_PATH_.'/config.php';
+require _APP_PATH_ . '/config.php';
 
 \One\Http\Router::loadRouter();
 
-try{
-    $req = new \One\Http\Request();
-    $res = new \One\Http\Response($req);
-    echo (new \One\Http\Router())->exec($req,$res);
-}catch (\One\Exceptions\HttpException $e){
+$req = new \One\Http\Request();
+$res = new \One\Http\Response($req);
+
+try {
+    $router = new \One\Http\Router();
+    list($req->class, $req->method, $mids, $action, $req->args) = $router->explain($req->method(), $req->uri(), $req, $res);
+    $f = $router->getExecAction($mids, $action, $res);
+    echo $f();
+} catch (\One\Exceptions\HttpException $e) {
     echo (new \App\Exceptions\Handler())->render($e);
-}catch (Exception $e){
+} catch (\One\Http\RouterException $e) {
+    echo \App\Exceptions\Handler::render(new \One\Exceptions\HttpException($res, $e->getMessage(), $e->getCode()));
+} catch (Exception $e) {
     echo $e->getMessage();
     \One\Facades\Log::debug($e);
 }
