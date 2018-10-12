27e35fa9e7b0ce3f03987a4530ab5730690cf437
DlenoDing <dleno@126.com>
2018-10-12 09:31:30 +0800

    ff

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -8,17 +8,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-
-        $name = $this->request->get('name');
-        if($name){
-            $this->server()->pushByName($name,rand(1,100));
-        }
-        return $this->json(['self_id' => $this->server()->worker_pid,
-            'pids' => $this->server()->getWorkerPids(),'fd' => $this->server()->_fd_name]);
-=======
         return $this->json($this->request->userAgent());
->>>>>>> 974c908ed80bd93e9eb79416b61c2a9eed6cf119
     }
 
     public function test($id1,$id2)
